a68d862352f5a41236574d408aded3f5c7568228
patrickatcw <pj8392@yahoo.com>
2017-10-11 12:53:07 -0400

    got a little more done with html and bootstrap

---
+++ app/data/friend.js
@@ -0,0 +1,21 @@
+//this file will contain all of our user data
+
+var characters = [{
+  routeName: "yoda",
+  name: "Yoda",
+  role: "Jedi Master",
+  age: 900,
+  forcePoints: 2000
+}, {
+  routeName: "darthmaul",
+  name: "Darth Maul",
+  role: "Sith Lord",
+  age: 200,
+  forcePoints: 1200
+}, {
+  routeName: "obiwankenobi",
+  name: "Obi Wan Kenobi",
+  role: "Jedi Master",
+  age: 55,
+  forcePoints: 1350
+}];
