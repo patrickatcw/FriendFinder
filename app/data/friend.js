8d78855543b776c97b3cc199c840c729af06f7e0
patrickatcw <pj8392@yahoo.com>
2017-10-12 11:36:57 -0400

    routes done

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,21 +1,33 @@
 //this file will contain all of our user data
 
-var characters = [{
-  routeName: "yoda",
-  name: "Yoda",
-  role: "Jedi Master",
-  age: 900,
-  forcePoints: 2000
+var friends = [{
+  routeName: "betty",
+  name: "Betty",
+  score: [1,4,3,5,2],
+  image src = "https://pbs.twimg.com/profile_images/1125351141/BettyWhite1.jpg"
+  height= 500, width= 500
+ }, {
+  routeName: "brad",
+  name: "Brad",
+  score: [5,5,3,1,1],
+  image src = "http://www.oztennisacademy.com/wp-content/uploads/2014/02/brad_profile22.jpg"
+  height= 500, width= 500
 }, {
-  routeName: "darthmaul",
-  name: "Darth Maul",
-  role: "Sith Lord",
-  age: 200,
-  forcePoints: 1200
+  routeName: "candice",
+  name: "Candice",
+  score: [1,1,1,5,5],
+  image src = "http://cdn-wpmsa.defymedia.com/wp-content/uploads/sites/3/2013/09/252941_180415822016588_171086099616227_486461_2431645_n.jpg"
+  height= 500, width= 500
 }, {
-  routeName: "obiwankenobi",
-  name: "Obi Wan Kenobi",
-  role: "Jedi Master",
-  age: 55,
-  forcePoints: 1350
+  routeName: "tina",
+  name: "Tina",
+  score: [3,5,3,2,1],
+ image src = "http://themepack.me/i/c/749x468/media/g/670/hot-girl-theme-bq17.jpg"
+  height= 500, width= 500
+  }, {
+  routeName: "toothy ted",
+  name: "Toothy Ted",
+  score: [5,5,5,5,5],
+  image src = "https://c1.staticflickr.com/9/8158/7679178698_e37fd9a200_b.jpg"
+  height= 500, width= 500
 }];
