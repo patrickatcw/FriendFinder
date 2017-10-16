d6a5b9ec06504b8368d98eea610cca2c3bdf17a3
patrickatcw <pj8392@yahoo.com>
2017-10-16 14:37:11 -0400

    getting betty to show up, but no one else, need to fix code

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,38 @@
 
-var friends = require("../data/friends");
-
+var friends = require("../data/friends")
 module.exports = function(app) {
- 
-  app.get("/api/friends", function(req, res) {
-    res.json(friends);
-  });
-  
-  app.post("/api/friends", function(req, res) {
-    console.log(req.body)
-    friends.push(req.body)
-    res.json(true)
-    
+    app.get("/api/friends", function(req, res){
+        res.json(friends);
     });
-  };
+    app.post("/api/friends", function(req, res){
+        var user = req.body;
+        var responses = user.scores;
+        console.log(user)
+        
+        for (var i = 0; i < friends.length; i++) {
+            var diff = 0
+            totalDif =[]
+            for (var j = 0; j < responses.length; j++) {
+            
+                diff += Math.abs(friends[i].scores[j] - responses[j])
+                totalDif.push(diff)
+            }
+        }
+        var index = 0;
+        var value = totalDif[4];
+        for (var i = 1; i < totalDif.length; i++) {
+            if (totalDif[i] < value) {
+                value = totalDif[i];
+                index = i;
+          }
+        }
+        var match = {
+            name: friends[index].name,
+            img: friends[index].image
+        };
+        res.json(match);
+        
+        console.log(match)
+        friends.push(user)
+    });
+};
