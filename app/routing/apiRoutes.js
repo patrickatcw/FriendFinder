2f5d7ca90c93e17594aa99e7cb9a56a312b4616d
patrickatcw <pj8392@yahoo.com>
2017-10-15 20:21:33 -0400

    need to work on for loops to go through friend matches

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,32 +1,16 @@
 
-var tableData = require("../data/tableData");
-var waitListData = require("../data/waitinglistData");
+var friends = require("../data/friends");
 
 module.exports = function(app) {
  
-  app.get("/api/tables", function(req, res) {
-    res.json(tableData);
-  });
-  app.get("/api/waitlist", function(req, res) {
-    res.json(waitListData);
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
   });
   
-  app.post("/api/tables", function(req, res) {
+  app.post("/api/friends", function(req, res) {
+    console.log(req.body)
+    friends.push(req.body)
+    res.json(true)
     
-    if (tableData.length < 5) {
-      tableData.push(req.body);
-      res.json(true);
-    }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
-  });
-  
-  app.post("/api/clear", function() {
-    
-    tableData = [];
-    waitListData = [];
-    console.log(tableData);
-  });
-};
+    });
+  };
